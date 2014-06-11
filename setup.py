71893ef31f9c0fda82a5f2821dc6b7a88e2bf939
svartalf <self@svartalf.info>
2014-06-11 12:50:51 +0900

    Added information about a python 3.3 and 3.4 support

--- setup.py
+++ setup.py
@@ -32,6 +32,8 @@ setup(
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: Implementation :: PyPy',
         'Topic :: Software Development :: Libraries',
     ),
